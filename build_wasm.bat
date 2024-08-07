a611181cd1a80c9dfbd113678f22a7ebd0287c16
Aronic (Tekkun) <aronic.tekkuniku@gmail.com>
2024-08-07 22:59:42 +0800

    Added assets folder and preload it

--- build_wasm.bat
+++ build_wasm.bat
@@ -9,4 +9,5 @@ set STACK_SIZE=1048576
 set HEAP_SIZE=67108864
 set c_files=src/web_release/main_wasm.c wasm_build/game.wasm.o raylib/wasm/libraylib.a
 set c_compile_flags=-sUSE_GLFW=3 -sGL_ENABLE_GET_PROC_ADDRESS -DWEB_BUILD -sSTACK_SIZE=%STACK_SIZE% -sALLOW_MEMORY_GROWTH -sERROR_ON_UNDEFINED_SYMBOLS=0 -sFORCE_FILESYSTEM -sEXIT_RUNTIME=1
-emcc -o wasm_build/index.html %c_files% %c_compile_flags%
+set preload_files=--preload-file assets
+emcc -o wasm_build/index.html %c_files% %c_compile_flags% %preload_files%
